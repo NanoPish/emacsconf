d5ad8031aff9a237f32cb04ecabe370d721e4e49
Paul Belloc <paul.belloc@epitech.eu>
2017-11-09 17:09:05 +0100

    webmode

--- init.el
+++ init.el
@@ -12,22 +12,23 @@
 ;; elisp loading
 (load "std.el")
 (load "std_comment.el")
-(load "php-mode")
-(load "html-helper-mode")
 
-;; Toggle between PHP & HTML Helper mode.  Useful when working on
-;; php files, that can been intertwined with HTML code
-(defun toggle-php-html-mode ()
-  (interactive)
-  "Toggle mode between PHP & HTML Helper modes"
-  (cond ((string= mode-name "HTML helper")
-	 (php-mode))
-	((string= mode-name "PHP")
-	 (html-helper-mode))))
-
-(global-set-key [f5] 'toggle-php-html-mode)
+;; web-mode start when open files
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 
 ;; line highlight
 (global-hl-line-mode 1)
 (set-face-background 'hl-line "#3e4446")
 (set-face-foreground 'highlight nil)
+
+;; MELPA packages 
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+		    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
