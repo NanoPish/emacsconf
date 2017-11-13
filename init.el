64c8148b559360a13d176d837d3a74a5d54c4179
Paul Belloc <paul.belloc@epitech.eu>
2017-11-13 17:03:26 +0100

    no splash, save files, light

--- init.el
+++ init.el
@@ -7,43 +7,52 @@
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
-(if (file-exists-p "~/.myemacs") 
-    (load-file "~/.myemacs"))
+;;MELPA
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+;; y or n instead of yes or no
+(fset 'yes-or-no-p 'y-or-n-p)
 
 ;; epitech
 (load "std.el")
 (load "std_comment.el")
-
-;; miniedit
-(load "miniedit.el")
-
-;; theme
-;; (load-theme 'solarized-dark t)
 
 ;; smart mode lines
 (setq sml/no-confirm-load-theme t)
 (setq sml/theme 'respectful)
 (sml/setup)
 
-;; web-mode start when open files
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+;; web-mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 ;; line highlight
 (global-hl-line-mode 1)
 (set-face-background 'hl-line "#3e4446")
 (set-face-foreground 'highlight nil)
 
-;; MELPA packages 
-(require 'package) ;; You might already have this line
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-		    (not (gnutls-available-p))))
-       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-  (add-to-list 'package-archives (cons "melpa" url) t))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+;; remove welcome GUI
+(setq inhibit-startup-screen t)
 
-;; y or n instead of yes or no
-(fset 'yes-or-no-p 'y-or-n-p)
+;; saves
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
