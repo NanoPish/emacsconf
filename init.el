3a1ac438642989efad50584bfa363d803bbe6667
Paul Belloc <paul.belloc@epitech.eu>
2017-12-07 15:43:17 +0100

    nlinum at startup + rem custom

--- init.el
+++ init.el
@@ -68,24 +68,10 @@
 ;; remove startup screen
 (setq inhibit-startup-screen t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (nlinum jedi projectile smart-mode-line better-defaults))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; nlinum
 (setq nlinum-format "%3d\u2502")
 (setq nlinum-highlight-current-line t)
+(global-nlinum-mode 1)
 
 ;; smooth scrolling
 (setq scroll-conservatively 10000)
